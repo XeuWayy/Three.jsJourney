ec1fc11abfcfe9847b699b530db0554f7a9b2b82
XeuWayy <corentin.gafam@proton.me>
2024-09-12 16:25:19 +0200

    Add ✨: Raging sea lessons.

--- 29.Raging sea/src/shaders/fragmentSea.glsl
+++ 29.Raging sea/src/shaders/fragmentSea.glsl
@@ -1,21 +1,3 @@
-/*
-uniform vec3 uDeptColor;
-uniform vec3 uSurfaceColor;
-
-uniform float uColorOffset;
-uniform float uColorMultiplier;
-
-varying float vElevation;
-
-void main() {
-    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
-    vec3 color = mix(uDeptColor, uSurfaceColor, mixStrength);
-
-    gl_FragColor = vec4(color, 1.0);
-    #include <colorspace_fragment>
-}
-*/
-
 uniform vec3 uDeptColor;
 uniform vec3 uSurfaceColor;
 
@@ -29,17 +11,13 @@ uniform float uFogFar;
 varying float vElevation;
 
 void main() {
-    // Couleur de base selon ton shader actuel
     float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
     vec3 color = mix(uDeptColor, uSurfaceColor, mixStrength);
 
-    // Calcul du brouillard
     float fogFactor = smoothstep(uFogNear, uFogFar, gl_FragCoord.z / gl_FragCoord.w);
 
-    // Mélange de la couleur finale avec la couleur du brouillard
     vec3 finalColor = mix(color, uFogColor, fogFactor);
 
-    // Appliquer la couleur finale avec une opacité de 1.0
     gl_FragColor = vec4(finalColor, 1.0);
 
     #include <colorspace_fragment>
